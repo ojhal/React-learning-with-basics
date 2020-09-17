720701141b2a86390a47106e4989fc72ccc62360
Ojhal Patwa <patkarojhal26@gmail.com>
2020-09-17 22:26:15 +0530

    console.log

--- src/App.js
+++ src/App.js
@@ -6,23 +6,23 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      name: "Tiger"
-    }
+    this.state = {}
     this.handleClick = this.handleClick.bind(this)
 
 
   }
-  handleClick() {
-    this.setState({ Animal: this.state.animal })
-  }
+  handleClick = () => {
+    this.setState({ name: "Tiger" });
+  };
 
   render() {
-    console.log("tiger")
+    console.log("Tiger")
     return (
       <div>
-        <button onClick={this.handleClick}> click me  </button>
-        <h1>{this.state.name}</h1>
+
+        <button onClick={this.handleClick}> Tiger </button>
+
+
 
       </div>
     );
